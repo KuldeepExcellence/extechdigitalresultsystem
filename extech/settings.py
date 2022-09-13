1ab91d177290220a8fd8132d7da66c331ec27111
Kuldeep Sharma <kuldeep.excellence@gmail.com>
2022-09-13 18:34:24 +0530

    gunicorn add

--- extech/settings.py
+++ extech/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-!jfuu18+$+sgojj$%joy-_zf=8(e!4od8+kiugrk_j*zh2jzcc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['extechdigitalresultsystem.herokuapp.com']
 
 
 
